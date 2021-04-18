dec5a2cc9cc7f79314de80fcabafc04f2d7fc5aa
jcarlosse <82538590+jcarlosse@users.noreply.github.com>
2021-04-18 14:34:04 -0500

    Tarea de factorial

---
+++ tarea3.js
@@ -0,0 +1,12 @@
+// tarea 3
+// Factorial
+
+var valorInicial =12
+var num = 1;
+
+for (var i = 1; i <= valorInicial; i++){
+
+    num *= i;
+    console.log("El factorial de " + i +" es: " +num);
+}    
+    
