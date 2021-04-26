8bce2913bc5393e3fcabac6aec8d27e98424f0c2
Juan Carlos Suárez <suarezpepsi@hotmail.com>
2021-04-25 23:07:12 -0500

    contar vocales

---
+++ tarea5.js
@@ -0,0 +1,46 @@
+// JavaScript source code
+// contar vocales
+
+var arreglo = ['murcielago'];
+
+function contarVocales(palabra) {
+
+    var largo = palabra.length;
+    var contA = 0, contE = 0, contI = 0, contO = 0, contU = 0, contCons = 0;
+
+    for (var i = 0; i < largo; i++) {
+        var letra = palabra.charAt(i).toLowerCase();
+
+        console.log(letra)
+
+        switch (letra) {
+
+            case 'a':                
+                contA++;
+                break;                
+            case 'e':
+                contE++;
+                break;
+            case 'i':
+                contI++;
+                break;
+            case 'o':
+                contO++;
+                break;
+            case 'u':
+                contU++;
+                break;
+            default:
+                contCons++;
+                break;
+
+
+        }//end of switch
+
+    }
+    return [contA, contE, contI, contO, contU, contCons];
+
+}
+
+console.log("Total de vocales a, e , i o, u y consonantes", contarVocales('murcielago'))
+
